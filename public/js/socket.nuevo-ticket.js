4a87881a90f652bf9b4b1645278e013dd7a8732f
Momfus <momfus@outlook.com>
2019-12-26 16:22:54 -0300

    Agregado el manejo de nuevos tickets - video 189

---
+++ public/js/socket.nuevo-ticket.js
@@ -0,0 +1,40 @@
+
+// Comando para establecer la conexión
+
+var socket = io();
+
+var label = $('#lblNuevoTicket'); // Id de un elemento de html de nuevo-ticket.html
+
+socket.on( 'connect', function(){
+
+    console.log('Conectado al servidor');
+    
+
+});
+
+
+socket.on( 'disconnect', function() {
+
+    console.log('Desconectado del servidor');
+    
+
+});
+
+// Crear un listener que este escuchando el evento estadoACtual de socket.js
+socket.on('estadoActual', function( res ){
+
+    console.log(res);
+    label.text( res.actual );
+
+});
+
+// Se escucha en el botón al hacer clik con jquery
+$('button').on('click', function() {
+
+    socket.emit('siguienteTicket', null, function( siguienteTicket ) {
+
+        label.text(siguienteTicket);
+
+    });
+
+});
--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -1,42 +1,32 @@
 const { io } = require('../server');
+const { TicketControl } = require('../classes/ticket-control.js');
 
+const ticketControl = new TicketControl();
 
 io.on('connection', (client) => {
 
-    console.log('Usuario conectado');
 
-    client.emit('enviarMensaje', {
-        usuario: 'Administrador',
-        mensaje: 'Bienvenido a esta aplicación'
-    });
+    client.on('siguienteTicket', ( data, callback ) => {
 
+        let siguiente = ticketControl.siguiente();
 
-
-    client.on('disconnect', () => {
-        console.log('Usuario desconectado');
-    });
-
-    // Escuchar el cliente
-    client.on('enviarMensaje', (data, callback) => {
-
-        console.log(data);
-
-        client.broadcast.emit('enviarMensaje', data);
-
-
-        // if (mensaje.usuario) {
-        //     callback({
-        //         resp: 'TODO SALIO BIEN!'
-        //     });
-
-        // } else {
-        //     callback({
-        //         resp: 'TODO SALIO MAL!!!!!!!!'
-        //     });
-        // }
-
-
+        console.log(siguiente);
+        callback(siguiente);
+        
 
     });
+
+    // Emitir un evento llamado 'estadoActual' que debe retornar el último ticket
+    /*
+        Debe retornar
+        {
+            actual: ticketControl.getUltimoTicket()   
+        }
+    */
+
+    client.emit('estadoActual', {
+                                actual: ticketControl.getUltimoTicket()   
+                                });
+
 
 });
